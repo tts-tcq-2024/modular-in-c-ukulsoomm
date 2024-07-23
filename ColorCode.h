8a871ed5d3a83550834c7a262471d8c77403dbac
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-07-23 11:17:38 +0530

    Update ColorCode.h

--- ColorCode.h
+++ ColorCode.h
@@ -1,1 +1,13 @@
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
