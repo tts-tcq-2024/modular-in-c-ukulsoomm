1bdf8ce3e930e2a0283893ca2e96b45a5012fd60
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-07-23 11:04:55 +0530

    Update ColorCode.h

--- EvenCountColorCode/ColorCode.h
+++ EvenCountColorCode/ColorCode.h
@@ -1,15 +1,17 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
