744e11d122f25b94b4af702edf5f46b770a7c564
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-07-24 20:26:26 +0530

    Update Test_EvenCountColorCode.c

--- Test_EvenCountColorCode.c
+++ Test_EvenCountColorCode.c
@@ -9,7 +9,7 @@ void testNumberToPair(int pairNumber,
     enum MajorColor expectedMajor,
     enum MinorColor expectedMinor)
 {
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    ColorPair colorPair = getColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     ColorPairToString(&colorPair, colorPairNames);
     printf("Got pair %s\n", colorPairNames);
@@ -25,7 +25,7 @@ void testPairToNumber(
     ColorPair colorPair;
     colorPair.majorColor = major;
     colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
+    int pairNumber = getPairNumberFromColor(&colorPair);
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
 }
