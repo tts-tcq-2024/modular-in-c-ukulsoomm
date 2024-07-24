e5110673c025a6fa695d9760a2ef711dec5cec67
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-07-24 19:48:09 +0530

    Update ColorPairToString.c

--- ColorPairToString.c
+++ ColorPairToString.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include EvenCountColorCode.h
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
