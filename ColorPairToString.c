d11f41e03cb49487805d7dffd2aa97b341e5fa7f
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-07-24 19:32:17 +0530

    Update ColorPairToString.c

--- ColorPairToString.c
+++ ColorPairToString.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <assert.h>
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
