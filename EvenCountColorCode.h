070b0dc9398f507ae6b16f2721eb3f25666517bf
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-07-24 19:39:50 +0530

    Update EvenCountColorCode.h

--- EvenCountColorCode.h
+++ EvenCountColorCode.h
@@ -2,3 +2,8 @@
 #define EVEN_COUNT_COLOR_CODE_H
 #inlcude ColorCode.h
 
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+
+#endif //EVEN_COUNT_COLOR_CODE_H
